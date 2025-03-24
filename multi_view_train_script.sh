49c75dbaca659831b6cd86174f44d928e6ab9b9d
klcodingtutor <kl.coding.tutor@gmail.com>
2025-03-24 13:55:33 +0000

    FIx

--- multi_view_train_script.sh
+++ multi_view_train_script.sh
@@ -11,7 +11,7 @@ LEARNING_RATE=0.001
 EPOCHS=20
 DROPOUT_RATE=0.5
 NUM_WORKERS=4
-DEVICE="CUDA"
+DEVICE="gpu"
 MODEL_SAVE_PATH="./models"
 
 # Create directories if they don't exist (no train/test subdirs)
