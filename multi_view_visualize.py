dd42450ad365d17ef9c3e9ce2de2afdd39e64778
klcodingtutor <kl.coding.tutor@gmail.com>
2025-03-25 05:28:14 +0000

    Testing 224 on CIFAR10

--- multi_view_visualize.py
+++ multi_view_visualize.py
@@ -54,6 +54,7 @@ def resize_image(img, size=(128, 128)):
 
 # Define test transform consistent with training
 test_transform = transforms.Compose([
+    transforms.Resize([224, 224]),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
