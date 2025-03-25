acec2416178e886d299b56dd3831747fa442f0c4
klcodingtutor <kl.coding.tutor@gmail.com>
2025-03-25 04:19:51 +0000

    Fix

--- multi_view_visualize.py
+++ multi_view_visualize.py
@@ -20,7 +20,7 @@ model = MultiViewAttentionCNN(
     drop_prob=args.dropout_rate,     # 0.5 from training script
     device=device
 )
-model_path = os.path.join(args.model_save_path, 'multi_view_attention_cnn_cifar10')
+model_path = os.path.join(args.model_save_path, 'multi_view_attention_cnn_cifar10.pth')
 model.load_state_dict(torch.load(model_path))
 model.to(device)
 model.eval()
