9693f6eba1f5ad65a251c4c6bf8b21e76ae9c3e7
klcodingtutor <kl.coding.tutor@gmail.com>
2025-03-25 05:32:32 +0000

    Fix bug on 224

--- multi_view_train_script.py
+++ multi_view_train_script.py
@@ -21,7 +21,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() and args.device == '
 train_transform = transforms.Compose([
     transforms.Resize([224, 224]),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(32, padding=4),
+    # transforms.RandomCrop(32, padding=4),
+    transforms.RandomRotation(15),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
